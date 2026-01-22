84613c429f12fa381672e916be5d00ecec7c3689
Boris Yuzhakov <boris.yuzhakov@lamoda.ru>
2026-01-22 22:03:32 +0300

    fix: dynamic robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from 'next'
 
 import { siteUrl } from '@/lib/seo/config'
 
+export const dynamic = 'force-static'
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
